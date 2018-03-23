fb213a041d60187bfdb66a885ec4f0d5b6c0fc3f
terry.liu <terry.liu@derbysoft.com>
2018-03-23 22:38:10 +0800

    add scss

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,42 +1,32 @@
 import React from 'react'
-import './index.less'
+import './index.css'
 import logo from '../../assets/images/logo.svg'
-import { connect } from 'react-redux'
-import { DECREMENT_REQUESTED, INCREMENT_REQUESTED } from '../../store/modules/login'
-import { NavBar, Icon,Button } from 'antd-mobile'
-//	<i class='iconfont  icon-msnui-tel user-icon'></i>
-const Login = props => {
-	// console.log(props, 'props')
-	let increment = () => {
-		props.dispatch({ type: INCREMENT_REQUESTED })
-	}
-	let decrement = () => {
-		props.dispatch({ type: DECREMENT_REQUESTED })
-	}
-	return (
-		<div className="pages">
-			<NavBar
-        mode="dark"
-        icon={<Icon type="left" />}>
-				登录
-      </NavBar>
-      <div className='login'>
-				<div className='user'>
-           <input type="text" />
-        </div>
-        <div className='password'>
-        <input type="text" />
-        </div>
-        <Button type="primary" className='sumbit'>登录</Button>
+const Login = () => (
+  <div className='login'>
+    <div className='title' />
+    <div className='coordinates-icon'>
+      <div className='coordinates topAct'>
+        <img src={logo} alt='logo' />
       </div>
-		</div>
-	)
-}
-
-const mapStateToProps = state => {
-	return {
-		...state,
-	}
-}
-
-export default connect(mapStateToProps)(Login)
+      <div className='circle-1-1 circle-show-2' />
+      <div className='circle-2-2 circle-show-1' />
+      <div className='circle-3-3 circle-show' />
+    </div>
+    <div className='login-form'>
+      <form action='#'>
+        <div className='user-pasw common-div'>
+          <span className='pasw-icon common-icon'>
+            <i className='iconfont  icon-miyao' />
+          </span>
+          <input type='password' name='password' v-model='accesstoken' placeholder='accesstoken' />
+        </div>
+        <div className='login-btn' >登录</div>
+      </form>
+    </div>
+    <div className='forgets'>
+      <router-link to='' />
+      <router-link to='/register'>注册账号>></router-link>
+    </div>
+  </div>
+)
+export default Login
